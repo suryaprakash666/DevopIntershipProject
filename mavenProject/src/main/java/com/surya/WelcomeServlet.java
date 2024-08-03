63cdbf94692fcfa835c2b0698d62019f61dde9f2
suryabhai <suryaprakashdas921@gmail.com>
2024-08-03 18:45:47 +0530

    Error fixed

--- mavenProject/src/main/java/com/surya/WelcomeServlet.java
+++ mavenProject/src/main/java/com/surya/WelcomeServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomepageServlet extends HttpServlet {
+public class WelcomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
