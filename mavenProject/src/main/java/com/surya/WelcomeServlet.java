6328d3b91d51ad33e7e4f0b71c3bd885cd2471c9
suryabhai <suryaprakashdas921@gmail.com>
2024-08-04 20:28:07 +0530

    Testing jenkins again

--- mavenProject/src/main/java/com/surya/WelcomeServlet.java
+++ mavenProject/src/main/java/com/surya/WelcomeServlet.java
@@ -5,14 +5,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 public class WelcomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("<html><body>");
-        response.getWriter().println("<h1>Welcome to the Maven Project!</h1>");
-        response.getWriter().println("</body></html>");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.html");
+        dispatcher.forward(request, response);
     }
 }
